7f4c42bfd330cc811f966d51b0fe8779662af804
nrchan <31267546+nrchan@users.noreply.github.com>
2020-12-05 20:55:55 +0800

    Update notes.py

--- notes.py
+++ notes.py
@@ -1,20 +1,32 @@
 import re
 
-note = r"([A-G])([#|x|b|bb])?"
+note = r"[A-G][#|x|bb|b]?"
 
 noteNumDict = {
-    "C"  : "0",
-    "B#" : "0",
-    "Dbb": "0",
-    "Bx" : "1",
-    "C#" : "1",
-    "Db" : "1",
-    "Cx" : "2",
-    "D"  : "2",
-    "Ebb": "2",
-    "D#" : "3",
-    "Eb" : "3",
-    "Fbb": "3",
+    "C"  : 0,
+    "B#" : 0,
+    "Dbb": 0,
+    "Bx" : 1,
+    "C#" : 1,
+    "Db" : 1,
+    "Cx" : 2,
+    "D"  : 2,
+    "Ebb": 2,
+    "D#" : 3,
+    "Eb" : 3,
+    "Fbb": 3,
+    "Dx" : 4,
+    "E"  : 4,
+    "Fb" : 4,
+    "E#" : 5,
+    "F"  : 5,
+    "Gbb": 5,
+    "Ex" : 6,
+    "F#" : 6,
+    "Gb" : 6,
+    "Fx" : 7,
+    "G"  : 7,
+    "Abb": 7,
 }
 
 #convert note to corresponding number, starting from C = 0, B = 12
