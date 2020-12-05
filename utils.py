1bc46b6a84f5fb913573ce145d871f9dfe75db6a
nrchan <31267546+nrchan@users.noreply.github.com>
2020-12-05 15:05:38 +0800

    Why isn't it working?

--- utils.py
+++ utils.py
@@ -1,35 +1,22 @@
 import os
 
-from dotenv import load_dotenv
-from linebot import LineBotApi
-from linebot.models import TextSendMessage, ImageSendMessage
-from linebot.exceptions import LineBotApiError
+from linebot import LineBotApi, WebhookParser
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 
-load_dotenv()
-FSM_GRAPH_URL = os.environ.get("FSM_GRAPH_URL")
-line_bot_api = LineBotApi(os.environ.get("LINE_CHANNEL_ACCESS_TOKEN"))
-
-def webhook_parser(webhook):
-    event = webhook["events"][0]
-    reply_token = event["replyToken"]
-    user_id = event["source"]["userId"]
-    message = event["message"]["text"]
-
-    return reply_token, user_id, message
+channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
 
 
-class LineAPI:
-    @staticmethod
-    def send_reply_message(reply_token, reply_msg):
-        try:
-            line_bot_api.reply_message(reply_token, TextSendMessage(text=reply_msg))
-        except LineBotApiError as e:
-            print(e)
+def send_text_message(reply_token, text):
+    line_bot_api = LineBotApi(channel_access_token)
+    line_bot_api.reply_message(reply_token, TextSendMessage(text=text))
 
-    def send_fsm_graph(reply_token):
-        try:
-            # for demo, hard coded image url, line api only support image over https
-            line_bot_api.reply_message(reply_token, ImageSendMessage(original_content_url=FSM_GRAPH_URL, preview_image_url=FSM_GRAPH_URL))
-        except LineBotApiError as e:
-            print(e)
+    return "OK"
+
+
+"""
+def send_image_url(id, img_url):
+    pass
+def send_button_message(id, text, buttons):
+    pass
+"""
