7196999c02316abb9829f0930993e75389072708
nrchan <31267546+nrchan@users.noreply.github.com>
2020-12-05 14:57:13 +0800

    switch to another ta's file

--- app.py
+++ app.py
@@ -1,67 +1,36 @@
 import os
-import sys
 import json
 
-from flask import Flask, jsonify, request, abort, send_file
+import requests
 from dotenv import load_dotenv
-from linebot import LineBotApi, WebhookParser
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
-from transitions.extensions import GraphMachine
+from flask import Flask, request, jsonify, send_from_directory
 
+from utils import LineAPI, webhook_parser
 from fsm import TocMachine
-from utils import send_text_message, webhook_parser
+
 
 load_dotenv()
-
+app = Flask(__name__, static_url_path='')
 machines = {}
 
-app = Flask(__name__, static_url_path="")
+
+# Handle State Trigger
+def handleTrigger(state, reply_token, user_id, text):
+    print("Server Handling State : %s" % state)
+    if state == "init":
+        machines[user_id].advance(reply_token, text)
+    if state == "options":
+        machines[user_id].choose_options(reply_token, text)
+    if state == "summation":
+        machines[user_id].enter_number(reply_token, text)
+
+@app.route('/', methods=['GET'])
+def reply():
+    return 'Hello, World!'
 
 
-# get channel_secret and channel_access_token from your environment variable
-channel_secret = os.getenv("LINE_CHANNEL_SECRET", None)
-channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
-if channel_secret is None:
-    print("Specify LINE_CHANNEL_SECRET as environment variable.")
-    sys.exit(1)
-if channel_access_token is None:
-    print("Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.")
-    sys.exit(1)
-
-line_bot_api = LineBotApi(channel_access_token)
-parser = WebhookParser(channel_secret)
-
-"""
-@app.route("/callback", methods=["POST"])
-def callback():
-    signature = request.headers["X-Line-Signature"]
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # parse webhook body
-    try:
-        events = parser.parse(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
-    # if event is MessageEvent and message is TextMessage, then echo text
-    for event in events:
-        if not isinstance(event, MessageEvent):
-            continue
-        if not isinstance(event.message, TextMessage):
-            continue
-
-        line_bot_api.reply_message(
-            event.reply_token, TextSendMessage(text=event.message.text)
-        )
-
-    return "OK"
-"""
-
-@app.route("/webhook", methods=["POST"])
-def webhook_handler():
+@app.route('/', methods=['POST'])
+def receive():
     webhook = json.loads(request.data.decode("utf-8"))
     reply_token, user_id, message = webhook_parser(webhook)
     print(reply_token, user_id, message)
@@ -69,46 +38,10 @@ def webhook_handler():
     if user_id not in machines:
         machines[user_id] = TocMachine()
 
-    response = machines[user_id].advance(message, reply_token)
-    if response == False:
-        send_text_message(reply_token, user_id)
-
+    handleTrigger(machines[user_id].state, reply_token, user_id, message)
     return jsonify({})
-    """
-    signature = request.headers["X-Line-Signature"]
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info(f"Request body: {body}")
 
-    # parse webhook body
-    try:
-        events = parser.parse(body, signature)
-    except InvalidSignatureError:
-        abort(400)
 
-    # if event is MessageEvent and message is TextMessage, then echo text
-    for event in events:
-        if not isinstance(event, MessageEvent):
-            continue
-        if not isinstance(event.message, TextMessage):
-            continue
-        if not isinstance(event.message.text, str):
-            continue
-        print(f"\nFSM STATE: {machine.state}")
-        print(f"REQUEST BODY: \n{body}")
-        response = machine.advance(event)
-        if response == False:
-            send_text_message(event.reply_token, event.source.user)
-    return "OK"
-    """
-
-"""
-@app.route("/show-fsm", methods=["GET"])
-def show_fsm():
-    machines.get_graph().draw("fsm.png", prog="dot", format="png")
-    return send_file("fsm.png", mimetype="image/png")
-"""
-
-if __name__ == "__main__":
-    port = os.environ.get("PORT", 8000)
-    app.run(host="0.0.0.0", port=port, debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host = '0.0.0.0', port = port)
