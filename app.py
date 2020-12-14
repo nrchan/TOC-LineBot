47069f9e9e8c0b0a8d1bf505e48cc0efea465c10
nrchan <31267546+nrchan@users.noreply.github.com>
2020-12-14 21:21:37 +0800

    fix

--- app.py
+++ app.py
@@ -66,8 +66,6 @@ def webhook_handler():
 @app.route("/show-fsm", methods=["GET"])
 def show_fsm():
     machine = TocMachine()
-    import os
-    os.system("dot -v")
     machine.get_graph().draw("fsm.png", prog="dot", format="png")
     return send_file("fsm.png", mimetype="image/png")
 
